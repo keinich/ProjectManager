ba12347279fbaa6882ec1b043eee0c95b9dae579
Rafael Weschle <rafael.weschle@gmail.com>
2021-05-26 17:11:05 +0200

    initial

---
+++ ProjectManager.Api/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectManager.Api.Controllers {
+
+  [ApiController]
+  [Route("api/home")]
+  public class HomeController : ControllerBase {
+    
+    [HttpGet]
+    public string Index() {
+      return "Hello World 2";
+    }
+  }
+
+}
